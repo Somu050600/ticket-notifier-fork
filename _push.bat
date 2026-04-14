01006f915265d5b0f02c85264e11f0788f196a1b
Ankur Vashishtha <bitstormer7819@gmail.com>
2026-04-14 15:16:16 +0530

    chore: update gitignore and cleanup

--- _push.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set GIT="C:\Program Files\Git\cmd\git.exe"
-cd /d "C:\Users\ANKUR VASHISHTHA\OneDrive\Documents\TicketAlert_v2\ticket-notifier"
-if exist ".git\index.lock" del /f ".git\index.lock"
-%GIT% add -A > git_output.log 2>&1
-%GIT% commit -m "refactor: cart-only mode + District queue handler" >> git_output.log 2>&1
-%GIT% push origin main >> git_output.log 2>&1
-echo DONE >> git_output.log
